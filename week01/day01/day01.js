6227dcd124557ef4b4789b43ebc859ba151907ad
calebeLAR <ca6ele@gmail.com>
2023-12-27 10:15:36 -0300

    FrankZammett.semana01.dia01: Table of Contents

---
+++ week01/day01/day01.js
@@ -0,0 +1,21 @@
+class Day01 {
+    constructor() {
+        this.day = 'dia01';
+        this.table_of_contents_title = 'Table of Contents';
+        this.topics = [
+            'About the Author',
+            'About_the_Technical_Reviewer',
+            'Acknowledgments',
+            'Introduction',
+        ];
+    }
+
+    show() {
+        for (const i of this.topics) {
+            console.log(i);
+        }
+    }
+}
+
+day01 = new Day01();
+day01.show();
